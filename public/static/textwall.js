bdbf5e96b819d2cdc700c0ca54a5052fd3d4ac83
Glitch (ehtw) <none>
2025-05-10 12:53:54 +0000

    💾💁 Checkpoint ./public/static/textwall.js:71769930/108 ./public/index.html:71769930/2928

--- public/static/textwall.js
+++ public/static/textwall.js
@@ -1861,7 +1861,7 @@ function convertToEmote(msg) {
           ar(r[0]),
             e["preventDefault"](),
             e["clipboardData"] || ir("Cut character.", 1e3);
-          Vn("")
+          Vn(" ", 0, !1, !0)
           var a = document["getElementById"]("copyico");
           (a["src"] = "/static/done.svg"),
             setTimeout(function () {
